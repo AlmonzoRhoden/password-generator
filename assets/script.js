ccaf8ae3d78fcf417c2f345372ca1bd0827bbb1d
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2020-12-30 10:52:24 -0500

    updating code for password generator

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+// Almonzo's JavaScript
+
+// Declaring variables for password
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
@@ -7,40 +10,34 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
 
-const randomFunc = {
+// Storing values in an object acessing key values
+const randomFunc = 
+{
 	lower: getRandomLower,
 	upper: getRandomUpper,
 	number: getRandomNumber,
 	symbol: getRandomSymbol
 }
 
-clipboard.addEventListener('click', () => {
-	const textarea = document.createElement('textarea');
-	const password = resultEl.innerText;
-	
-	if(!password) { return; }
-	
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand('copy');
-	textarea.remove();
-	alert('Password copied to clipboard');
-});
-
-generate.addEventListener('click', () => {
+// Adding an event listener on + generic function to specfied id tags for password generation
+// Re-wrote code from arrow functions to regular
+addEventListener('click', function ()
+{
 	const length = +lengthEl.value;
 	const hasLower = lowercaseEl.checked;
 	const hasUpper = uppercaseEl.checked;
 	const hasNumber = numbersEl.checked;
 	const hasSymbol = symbolsEl.checked;
 	
+	// Showing generated password to screen
 	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
 	let generatedPassword = '';
+	// Storing choices 
 	const typesCount = lower + upper + number + symbol;
+	// Putting values into an arry to be accessed
 	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
 	
 	// Doesn't have a selected type
@@ -48,7 +45,7 @@ function generatePassword(lower, upper, number, symbol, length) {
 		return '';
 	}
 	
-	// create a loop
+	// Creating a for loop
 	for(let i=0; i<length; i+=typesCount) {
 		typesArr.forEach(type => {
 			const funcName = Object.keys(type)[0];
@@ -61,19 +58,26 @@ function generatePassword(lower, upper, number, symbol, length) {
 	return finalPassword;
 }
 
-function getRandomLower() {
+// Getting a random value using Math.floor + Math. Random,
+// Storing it in functions below to be re-used in program.
+
+function getRandomLower() 
+{
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-function getRandomUpper() {
+function getRandomUpper() 
+{
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getRandomNumber() {
+function getRandomNumber()
+{
 	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-function getRandomSymbol() {
+function getRandomSymbol() 
+{
 	const symbols = '!@#$%^&*(){}[]=<>/,.'
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
